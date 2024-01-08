4b0ac6058747c0f0e2e7176200f6c4d7ecbbf31b
06-shuu <shumokh@aol.com>
2024-01-08 16:25:00 +0300

    update res folder

--- Chapter02/src/Example2-11.cpp
+++ Chapter02/src/Example2-11.cpp
@@ -1,8 +1,13 @@
+//A complete program to read in a color video and write out the log-polartransformed video
+
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
 int main() {
-	 
+	std::string window1="Example 2-11", window2="Log Polar";
+	cv::namedWindow(window1,cv::WINDOW_AUTOSIZE);
+	cv::namedWindow(window2,cv::WINDOW_AUTOSIZE);
+
 
 	return 0;
 }
