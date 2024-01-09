a833307379934ea14442854f45137f4461b300c7
06-shuu <shumokh@aol.com>
2024-01-09 13:58:04 +0300

    ch02 exercise5 - done

--- Chapter02/src/Exercise5.cpp
+++ Chapter02/src/Exercise5.cpp
@@ -6,21 +6,16 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
+//global var
 int downsamplingLevel = 2;
+//func. prototype
 void onTrackbarSlide(int factor, void *);
 
 int main() {
 	std::string window1 = "Downsampling";
 	cv::namedWindow(window1, cv::WINDOW_AUTOSIZE);
 	
-
 	cv::VideoCapture cap("res/videos/Moriarty.mp4");
-	double fps = cap.get(cv::CAP_PROP_FPS);
-	cv::Size size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH),
-		(int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-
-	//cv::VideoWriter writer;
-	//writer.open("res/saved/savedMoriarty.avi", CV_FOURCC('M', 'J', 'P', 'G'), fps, size);
 
 	cv::createTrackbar("Downsampling level", window1, &downsamplingLevel, 8, onTrackbarSlide); 
 
@@ -33,8 +28,7 @@ int main() {
 		//downsampling the video and display it and save it
 		cv::pyrDown(frame, frame2);
 		cv::imshow(window1, frame2);
-		
-		//writer << frame2;
+	
 		char c = cv::waitKey(10);
 		if (c == 27)
 			break;
